931f48ce8d781f6a6e0d9a6ad50f7ab8f590b8f8
Jose Daniel Hurtado <josedaniel0227@hotmail.com>
2018-07-23 13:12:48 -0400

    Add Player/Enemy functionality

--- js/app.js
+++ js/app.js
@@ -1,39 +1,81 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+let Enemy = function(x, y) {
+  this.x = x ;
+  this.y = y;
+  this.speed = 100 + Math.floor(Math.random() * 128);
+  this.sprite = 'images/enemy-bug.png';
 };
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Regulates constant speed on any device, also used as the collision detection. Called by the Engine.js file.
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  this.x += this.speed * dt;
+   if (this.x > 500) {
+     this.x = -150 ;
+   }
+
 };
 
-// Draw the enemy on the screen, required method for game
+
+// Draw Enemies based on updated x and y coordiantes.
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+//Array that holds enemies
+var enemy1 = new Enemy(0,68 );
+var enemy2 = new Enemy(0,68 );
+var enemy3 = new Enemy(0,151);
+var enemy4 = new Enemy(0,151);
+var enemy5 = new Enemy(0,234);
+var enemy6 = new Enemy(0,234);
+var allEnemies = [];
+allEnemies.push(enemy1,enemy2,enemy3,enemy4,enemy5,enemy6);
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+/*
+/Player
+*/
+
+let Player = function() {
+  this.x = 101 * 2;
+  this.y = 83 * 5 - 10;
+  this.sprite = 'images/char-boy.png';
+};
+Player.prototype.update = function() {
+  // this.x = this.x;
+  // this.y = this.y;
+};
 
 
+Player.prototype.handleInput = function(dt) {
+  switch (dt) {
+  case "up":
+    if (this.y > 0){
+    this.y -= 83;
+    }
+    break;
+  case "down":
+    if (this.y < 400){
+      this.y += 83;
+    }
+    break;
+  case "left":
+  if (this.x > 0) {
+    this.x -= 101;
+  }
+    break;
+  case "right":
+  if (this.x < 400 ){
+    this.x += 101;
+}
+    break;
+}
+};
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+let player = new Player();
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
--- js/engine.js
+++ js/engine.js
@@ -9,7 +9,7 @@
  * drawn but that is not the case. What's really happening is the entire "scene"
  * is being drawn over and over, presenting the illusion of animation.
  *
- * This engine makes the canvas' context (ctx) object globally available to make 
+ * This engine makes the canvas' context (ctx) object globally available to make
  * writing app.js a little simpler to work with.
  */
 
@@ -77,9 +77,14 @@ var Engine = (function(global) {
      * functionality this way (you could just implement collision detection
      * on the entities themselves within your app.js file).
      */
+     function checkCollisions(){
+       // return (Math.abs(this.y - player.y) === 10) && (Math.abs(this.x - player.x) < 75);
+       // console.log("collision")
+
+     };
     function update(dt) {
         updateEntities(dt);
-        // checkCollisions();
+        checkCollisions();
     }
 
     /* This is called by the update function and loops through all of the
@@ -117,7 +122,7 @@ var Engine = (function(global) {
             numRows = 6,
             numCols = 5,
             row, col;
-        
+
         // Before drawing, clear existing canvas
         ctx.clearRect(0,0,canvas.width,canvas.height)
 
